0e1807c8b8eca7829f1068e3575d0de0ade2a1f8
carlos <desoftsis@gmail.com>
2024-11-01 16:10:11 -0400

    Segundo intento

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://carlosvpinto.github.io',
-  base: '/DolaralDia/'
+  site: 'https://www.dolaraldiavzla.com',
+  base: '/carlosvpinto'
 });
 
