28c94a0554ebcb1c67a049ac0f05ea2cc1667f0f
carlos <desoftsis@gmail.com>
2024-11-04 14:18:08 -0400

    Otro mas 2  4-11-24

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://www.dolaraldiavzla.com',
-  base: '/carlosvpinto'
+  site: 'https://www.dolaraldiavzla.com', // Tu dominio principal
+  base: '/paginaastro', // El nombre de tu repositorio en GitHub
 });
-
