7c9bcf12a4b10237de5536ccf2f6b6065f35a694
carlos <desoftsis@gmail.com>
2024-11-04 23:28:21 -0400

    Otro mas 10 ya con dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://www.dolaraldiavzla.com', // Tu dominio personalizado
-  base: '/paginaastro', // Nombre del repositorio en GitHub Pages
+  base: '/', // Nombre del repositorio en GitHub Pages
 });
