ffe999b24639ad003693136547ec33c9b74e2c60
carlos <desoftsis@gmail.com>
2024-11-06 14:55:05 -0400

    Otro mas 10 ya con  menu

---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,27 @@
+import type { MenuItem } from "../types/types";
+
+// Modificación de URLs para usar anclas dentro de la misma página
+const menuItems: MenuItem[] = [
+  {
+    title: "Home",
+    url: "#home",  // Enlace a la sección con id="home"
+    icon: "",
+  },
+  {
+    title: "Paginas",
+    url: "#paginas",  // Enlace a la sección con id="paginas"
+    icon: "",
+  },
+  {
+    title: "Tasa Bancos",
+    url: "#bancos",  // Enlace a la sección con id="paginas"
+    icon: "",
+  },
+  {
+    title: "Usuarios",
+    url: "#usuarios",  // Enlace a la sección con id="usuarios"
+    icon: "",
+  },
+];
+
+export default menuItems;
